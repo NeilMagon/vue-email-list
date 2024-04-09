a63c9b00aadd1afe80dfcad2a346962878dc1a73
NeilMagon <neil.magon97@gmail.com>
2024-04-09 17:24:28 +0200

    feat: add bonus

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ createApp({
     },
     methods: {
         getEmails(){
-            for(let i = 0; i < 10; i++){
+            for(let i = 1; i <= 10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').
                 then((response) => {
                     this.emails.push(response.data.response);
